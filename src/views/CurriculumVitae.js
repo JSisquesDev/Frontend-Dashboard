17cda69e5f52ecfcc3ffb15324c1558dd5df2239
Javier Plaza Sisqués <javierplazasisques@gmail.com>
2023-12-31 13:17:12 +0100

    Update Curriculum.js

--- src/views/CurriculumVitae.js
+++ src/views/CurriculumVitae.js
@@ -54,7 +54,9 @@ function CurriculumVitae(props) {
                         Javier Plaza Sisqués
                         <br></br>
                         <p>
-                          <small className="text-muted">Programador</small>
+                          <small className="text-muted">
+                            <i>Programador</i>
+                          </small>
                         </p>
                       </h2>
                     </Row>
@@ -114,17 +116,54 @@ function CurriculumVitae(props) {
             <Card style={{ height: "20rem" }} className="card-chart">
               <CardHeader>
                 <h5 className="card-category">Estudios</h5>
-                <CardTitle tag="h3">
-                  <i className="tim-icons icon-send text-success" /> 12,100K
-                </CardTitle>
               </CardHeader>
               <CardBody>
-                <div className="chart-area">
-                  <Line
-                    data={chartExample4.data}
-                    options={chartExample4.options}
-                  />
-                </div>
+                <Row>
+                  <Col lg="4">
+                    <h6>Estudios Generales</h6>
+                    <p>Divina Pastora</p>
+                    <small className="text-muted">
+                      <i>2002-2015</i>
+                    </small>
+                  </Col>
+                  <Col lg="4">
+                    <h6>DAM</h6>
+                    <p>Universidad Europea</p>
+                    <small className="text-muted">
+                      <i>2018-2020</i>
+                    </small>
+                  </Col>
+                  <Col lg="4">
+                    <h6>Curso Ciberseguridad</h6>
+                    <p>Incibe</p>
+                    <small className="text-muted">
+                      <i>2016</i>
+                    </small>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg="4">
+                    <h6>SMR</h6>
+                    <p>I.E.S. San Andrés</p>
+                    <small className="text-muted">
+                      <i>2016-2018</i>
+                    </small>
+                  </Col>
+                  <Col lg="4">
+                    <h6>Ingeniería Informática</h6>
+                    <p>Universidad Europea</p>
+                    <small className="text-muted">
+                      <i>2020-Actualmente</i>
+                    </small>
+                  </Col>
+                  <Col lg="4">
+                    <h6>NDG Linux Essentials</h6>
+                    <p>Cisco</p>
+                    <small className="text-muted">
+                      <i>2019</i>
+                    </small>
+                  </Col>
+                </Row>
               </CardBody>
             </Card>
           </Col>
@@ -192,17 +231,38 @@ function CurriculumVitae(props) {
             <Card style={{ height: "20rem" }} className="card-chart">
               <CardHeader>
                 <h5 className="card-category">Idiomas</h5>
-                <CardTitle tag="h3">
-                  <i className="tim-icons icon-bell-55 text-info" /> 763,215
-                </CardTitle>
               </CardHeader>
               <CardBody>
-                <div className="chart-area">
-                  <Line
-                    data={chartExample2.data}
-                    options={chartExample2.options}
-                  />
-                </div>
+                <Row>
+                  <Col>
+                    <i className="tim-icons icon-single-02" />
+                  </Col>
+                  <Col>
+                    <Row>
+                      <p>Español</p>
+                    </Row>
+                    <Row>
+                      <small className="text-muted">
+                        <i>Nativo</i>
+                      </small>
+                    </Row>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <i className="tim-icons icon-single-02" />
+                  </Col>
+                  <Col>
+                    <Row>
+                      <p>Inglés</p>
+                    </Row>
+                    <Row>
+                      <small className="text-muted">
+                        <i>B2</i>
+                      </small>
+                    </Row>
+                  </Col>
+                </Row>
               </CardBody>
             </Card>
           </Col>
@@ -210,17 +270,44 @@ function CurriculumVitae(props) {
             <Card style={{ height: "20rem" }} className="card-chart">
               <CardHeader>
                 <h5 className="card-category">Premios</h5>
-                <CardTitle tag="h3">
-                  <i className="tim-icons icon-bell-55 text-info" /> 763,215
-                </CardTitle>
               </CardHeader>
               <CardBody>
-                <div className="chart-area">
-                  <Line
-                    data={chartExample2.data}
-                    options={chartExample2.options}
-                  />
-                </div>
+                <Row>
+                  <Col className="font-icon-list" xs="1">
+                    <i className="tim-icons icon-single-02" />
+                  </Col>
+                  <Col className="font-icon-list">
+                    <p>5ª posición concurso regional programame</p>
+                    <small className="text-muted">
+                      <i>2019</i>
+                    </small>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="font-icon-list" xs="1">
+                    <i className="tim-icons icon-single-02" />
+                  </Col>
+                  <Col className="font-icon-list">
+                    <p>Finalista competición nacional programame</p>
+                    <small className="text-muted">
+                      <i>2019</i>
+                    </small>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="font-icon-list" xs="1">
+                    <i className="tim-icons icon-single-02" />
+                  </Col>
+                  <Col className="font-icon-list">
+                    <p>
+                      1º premio al mejor PBS (Project Based School) de ciclos
+                      formativos de grado superior
+                    </p>
+                    <small className="text-muted">
+                      <i>2019</i>
+                    </small>
+                  </Col>
+                </Row>
               </CardBody>
             </Card>
           </Col>
@@ -228,17 +315,35 @@ function CurriculumVitae(props) {
             <Card style={{ height: "20rem" }} className="card-chart">
               <CardHeader>
                 <h5 className="card-category">Otros</h5>
-                <CardTitle tag="h3">
-                  <i className="tim-icons icon-bell-55 text-info" /> 763,215
-                </CardTitle>
               </CardHeader>
               <CardBody>
-                <div className="chart-area">
-                  <Line
-                    data={chartExample2.data}
-                    options={chartExample2.options}
-                  />
-                </div>
+                <Row>
+                  <Col>
+                    <h6>Vocal suplente consejo de delegados</h6>
+                    <p>Universidad Europea</p>
+                    <small className="text-muted">
+                      <i>2018-2019</i>
+                    </small>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <h6>Delegado de clase</h6>
+                    <p>Universidad Europea</p>
+                    <small className="text-muted">
+                      <i>2018-2020</i>
+                    </small>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <h6>Permiso de conducir</h6>
+                    <p>Código B</p>
+                    <small className="text-muted">
+                      <i>2018</i>
+                    </small>
+                  </Col>
+                </Row>
               </CardBody>
             </Card>
           </Col>
